aee152be9a7bbb129cce7e88d2fdd058470fc141
Rashmit Chudasama <rashmit.chudasama@THIRDI.COM>
2018-03-14 15:18:46 +0530

    Java project salary slip automation tool

---
+++ SalarySlip _Automater/src/com/thirdi/excelpoc/reader/EmployeeLeaves.java
@@ -0,0 +1,121 @@
+package com.thirdi.excelpoc.reader;
+
+public class EmployeeLeaves {
+	 
+	private String presentDaysV= "-"; //15
+	private String AlAvailedV= "-"; //16
+	private String BlAvailedV= "-"; // 17
+	private String compOffV= "-"; // 18
+	private String wfhV= "-"; // 19
+	private String optionalHolidayV= "-"; // 20
+	private String holidayV= "-"; //21
+	private String peternityLeaveV= "-"; //22
+	private String maternityLeave= "-"; // 23
+	private String leaveWOPayV= "-"; //  24 
+	private String weeklyOffV= "-"; //  26
+	private String salaryPaidDaysV= "-"; //  27
+	private String openingBalanceALV= "-"; // 52
+	private String additionalALV= "-"; // 53
+	private String closingBalanceV= "-"; // 54
+	private String bereavementLeaveBalV= "-";// 55
+	
+	
+	public String getPresentDaysV() {
+		return presentDaysV;
+	}
+	public void setPresentDaysV(String presentDaysV) {
+		this.presentDaysV = presentDaysV;
+	}
+	public String getAlAvailedV() {
+		return AlAvailedV;
+	}
+	public void setAlAvailedV(String alAvailedV) {
+		AlAvailedV = alAvailedV;
+	}
+	public String getBlAvailedV() {
+		return BlAvailedV;
+	}
+	public void setBlAvailedV(String blAvailedV) {
+		BlAvailedV = blAvailedV;
+	}
+	public String getCompOffV() {
+		return compOffV;
+	}
+	public void setCompOffV(String compOffV) {
+		this.compOffV = compOffV;
+	}
+	public String getWfhV() {
+		return wfhV;
+	}
+	public void setWfhV(String wfhV) {
+		this.wfhV = wfhV;
+	}
+	public String getOptionalHolidayV() {
+		return optionalHolidayV;
+	}
+	public void setOptionalHolidayV(String optionalHolidayV) {
+		this.optionalHolidayV = optionalHolidayV;
+	}
+	public String getLeaveWOPayV() {
+		return leaveWOPayV;
+	}
+	public void setLeaveWOPayV(String leaveWOPayV) {
+		this.leaveWOPayV = leaveWOPayV;
+	}
+	public String getHolidayV() {
+		return holidayV;
+	}
+	public void setHolidayV(String holidayV) {
+		this.holidayV = holidayV;
+	}
+	public String getWeeklyOffV() {
+		return weeklyOffV;
+	}
+	public void setWeeklyOffV(String weeklyOffV) {
+		this.weeklyOffV = weeklyOffV;
+	}
+	public String getSalaryPaidDaysV() {
+		return salaryPaidDaysV;
+	}
+	public void setSalaryPaidDaysV(String salaryPaidDaysV) {
+		this.salaryPaidDaysV = salaryPaidDaysV;
+	}
+	public String getClosingBalanceV() {
+		return closingBalanceV;
+	}
+	public void setClosingBalanceV(String closingBalanceV) {
+		this.closingBalanceV = closingBalanceV;
+	}
+	public String getMaternityLeave() {
+		return maternityLeave;
+	}
+	public void setMaternityLeave(String maternityLeave) {
+		this.maternityLeave = maternityLeave;
+	}
+	public String getBereavementLeaveBalV() {
+		return bereavementLeaveBalV;
+	}
+	public void setBereavementLeaveBalV(String bereavementLeaveBalV) {
+		this.bereavementLeaveBalV = bereavementLeaveBalV;
+	}
+	public String getOpeningBalanceALV() {
+		return openingBalanceALV;
+	}
+	public void setOpeningBalanceALV(String openingBalanceALV) {
+		this.openingBalanceALV = openingBalanceALV;
+	}
+	public String getAdditionalALV() {
+		return additionalALV;
+	}
+	public void setAdditionalALV(String additionalALV) {
+		this.additionalALV = additionalALV;
+	}
+	public String getPeternityLeaveV() {
+		return peternityLeaveV;
+	}
+	public void setPeternityLeaveV(String peternityLeaveV) {
+		this.peternityLeaveV = peternityLeaveV;
+	}
+    	
+	
+}
